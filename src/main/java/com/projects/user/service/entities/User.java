faf1531ce6a5637f6cc7e3925ea8df9cafa979b7
Rishi Raj Chansauria <rishi.chansauria@gmail.com>
2024-12-12 22:30:49 +0530

    Updated user service to resolve lombok dependency issue

--- src/main/java/com/projects/user/service/entities/User.java
+++ src/main/java/com/projects/user/service/entities/User.java
@@ -3,8 +3,15 @@ package com.projects.user.service.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class User {
 
@@ -16,27 +23,4 @@ public class User {
     @Column(name = "PASSWORD")
     private String password;
 
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
--- src/main/java/com/projects/user/service/payload/ApiResponse.java
+++ src/main/java/com/projects/user/service/payload/ApiResponse.java
@@ -1,41 +1,15 @@
 package com.projects.user.service.payload;
 
-import lombok.Builder;
+import lombok.*;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 public class ApiResponse {
     private String message;
     private boolean success;
     private HttpStatus status;
-
-    public ApiResponse(String message, boolean success, HttpStatus status) {
-        this.message = message;
-        this.success = success;
-        this.status = status;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public boolean isSuccess() {
-        return success;
-    }
-
-    public void setSuccess(boolean success) {
-        this.success = success;
-    }
-
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(HttpStatus status) {
-        this.status = status;
-    }
 }
