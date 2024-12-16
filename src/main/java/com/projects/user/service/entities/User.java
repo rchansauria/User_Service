e3d2ad72202920956dd1b4f50374faefe9cae767
Rishi Raj Chansauria <rishi.chansauria@gmail.com>
2024-12-16 20:53:10 +0530

    Developed communication between microservices

--- src/main/java/com/projects/user/service/entities/User.java
+++ src/main/java/com/projects/user/service/entities/User.java
@@ -3,10 +3,13 @@ package com.projects.user.service.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -22,5 +25,8 @@ public class User {
     private String username;
     @Column(name = "PASSWORD")
     private String password;
+
+    @Transient
+    private List<Rating> ratings;
 
 }
--- src/main/java/com/projects/user/service/services/impl/UserServiceImpl.java
+++ src/main/java/com/projects/user/service/services/impl/UserServiceImpl.java
@@ -1,20 +1,29 @@
 package com.projects.user.service.services.impl;
 
+import com.projects.user.service.entities.Hotel;
+import com.projects.user.service.entities.Rating;
 import com.projects.user.service.entities.User;
 import com.projects.user.service.exceptions.ResourceNotFoundException;
 import com.projects.user.service.repositories.UserRepository;
 import com.projects.user.service.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Override
     public User createUser(User user) {
@@ -30,7 +39,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUserById(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User not found"));
+        Rating[] ratings = restTemplate.getForObject("http://RATINGSERVICE/ratings/user/" + user.getUserId(), Rating[].class);
+        List<Rating> ratingList = Arrays.stream(ratings).toList();
+        List<Rating> ratingNewList = ratingList.stream().map(rating -> {
+           ResponseEntity<Hotel> hotel =  restTemplate.getForEntity("http://HOTELSERVICE/hotels/" + rating.getHotelId(), Hotel.class);
+           rating.setHotel(hotel.getBody());
+           return rating;
+        }).collect(Collectors.toList());
+        user.setRatings(ratingNewList);
+        return user;
     }
 
     @Override
